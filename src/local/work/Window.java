2c20708f7b9b6c87f296b35ab4d426a6319afa31
James Haining <hipster1911@gmail.com>
2025-12-17 12:13:27 -0600

    Remove unnecessary comments from Window class

--- src/local/work/Window.java
+++ src/local/work/Window.java
@@ -9,7 +9,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Window extends JPanel{
-//    private static JLabel label;
+
     private static Toolbar toolbar;
     private static DisplayArea label;
     private static FileTreeArea fileTreeArea;
@@ -20,9 +20,9 @@ public class Window extends JPanel{
         Window.label = new DisplayArea();
         Window.fileTreeArea = new FileTreeArea();
         Window.propertiesArea = new PropertiesArea();
+
         this.setLayout(new BorderLayout());
-//        Window.label = new JLabel("Hello World");
-//        label.setAlignmentY(0.5f);
+
         this.add(toolbar, BorderLayout.NORTH);
         this.add(label, BorderLayout.EAST);
         this.add(fileTreeArea, BorderLayout.WEST);
